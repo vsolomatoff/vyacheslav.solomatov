31c79967ed510357903cad5f9d91d7d835d6ec18
Solomatov Viacheslav <solomatoff.studio@gmail.com>
2020-12-14 16:14:09 +0500

    1.2. Режим ожидания. [#430969]

---
+++ chapter_007/src/main/java/Wget.java
@@ -0,0 +1,19 @@
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    for (int index=0; index<=100; index++) {
+                        try {
+                            System.out.print("\rLoading : " + index  + "%");
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+        );
+        thread.start();
+        System.out.println("Main");
+    }
+}
