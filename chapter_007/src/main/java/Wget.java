f0d4d53dabc84dff508c8ce8f0f8c61c8a297c8e
Solomatov Viacheslav <solomatoff.studio@gmail.com>
2020-12-14 18:55:34 +0500

    4. Скачивание файла с ограничением. [#430967]

--- chapter_007/src/main/java/Wget.java
+++ chapter_007/src/main/java/Wget.java
@@ -1,19 +1,52 @@
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    for (int index=0; index<=100; index++) {
-                        try {
-                            System.out.print("\rLoading : " + index  + "%");
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
+import java.util.Arrays;
 
-                }
-        );
-        thread.start();
-        System.out.println("Main");
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        FileDownload fileDownload = new FileDownload();
+        try {
+            fileDownload.fileDownload(this.url, this.speed);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Started Wget.main");
+        System.out.println("args = " + Arrays.deepToString(args));
+
+        String url;
+        if (args.length>0) {
+            url = args[0];
+            if (url.isEmpty()) {
+                url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+            }
+        } else {
+            url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        }
+
+        int speed = 5000;
+        if (args.length>1) {
+            if (!args[1].isEmpty()) {
+                speed = Integer.parseInt(args[1]);
+            }
+        }
+
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        try {
+            wget.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Finished Wget.main");
     }
 }
