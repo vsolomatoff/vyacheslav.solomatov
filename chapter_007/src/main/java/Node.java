5eaa99a466b5efcd62f61dbd89252921ae7dae58
Solomatov Viacheslav <solomatoff.studio@gmail.com>
2020-12-15 20:05:55 +0500

    3. Immutable объекты [#430976]

---
+++ chapter_007/src/main/java/Node.java
@@ -0,0 +1,28 @@
+import net.jcip.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public final class Node<T> {
+    private final Node next;
+    private final T value;
+
+    public Node(Node next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "next=" + next +
+                ", value=" + value +
+                '}';
+    }
+}
