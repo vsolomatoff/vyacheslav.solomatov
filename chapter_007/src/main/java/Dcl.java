eb59760148b536509bc3b53456eb9e5b5721b094
Solomatov Viacheslav <solomatoff.studio@gmail.com>
2020-12-15 19:36:05 +0500

    2. Модель памяти Java [#430975]

---
+++ chapter_007/src/main/java/Dcl.java
@@ -0,0 +1,12 @@
+public class Dcl {
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(DCLSingleton::instOf);
+        Thread second = new Thread(DCLSingleton::instOf);
+        first.start();
+        //Thread.sleep(3000);
+        second.start();
+        first.join();
+        second.join();
+        System.out.println("DCLSingleton.instOf() = " + DCLSingleton.instOf());
+    }
+}
