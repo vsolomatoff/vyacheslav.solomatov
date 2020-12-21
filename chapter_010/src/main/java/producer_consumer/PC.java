59f9d304b9f4d184f3f14eb8ded68d76b5c2fecd
Solomatov Viacheslav <solomatoff.studio@gmail.com>
2020-12-21 15:47:36 +0500

    Разные примеры программ

---
+++ chapter_010/src/main/java/producer_consumer/PC.java
@@ -0,0 +1,10 @@
+package producer_consumer;
+
+public class PC {
+    public static void main(String args[]) {
+        Q q = new Q();
+        new Producer(q);
+        new Consumer(q);
+        System.out.println("Для остановки нажмите Ctrl+C.");
+    }
+}
