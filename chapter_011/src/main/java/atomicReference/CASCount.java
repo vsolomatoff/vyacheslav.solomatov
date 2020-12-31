e04b69fa193e676a12d33a935c47275e2498e6b2
Solomatov Viacheslav <solomatoff.studio@gmail.com>
2020-12-31 16:15:37 +0500

    0. CAS - операции [#430990]

--- chapter_011/src/main/java/atomicReference/CASCount.java
+++ chapter_011/src/main/java/atomicReference/CASCount.java
@@ -2,26 +2,23 @@ package atomicReference;
 
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @ThreadSafe
 public class CASCount {
-    private final AtomicReference<Integer> count = new AtomicReference<>(0);
+    private final AtomicInteger count = new AtomicInteger(0);
 
     public void increment() {
-        while(true) {
+        do {
             int existingValue = get();
             int newValue = existingValue + 1;
-            if(count.compareAndSet(existingValue, newValue)) {
+            if (count.compareAndSet(existingValue, newValue)) {
                 return;
             }
-        }
+        } while (true);
     }
 
     public int get() {
-        //System.out.println("Started CASCount.get");
-        //System.out.println("    count = " + count);
-        //System.out.println("    count.get() = " + count.get());
         return count.get();
     }
 }
