66ac788de17439ba7f0218bffc3befa43f902fda
Solomatov Viacheslav <solomatoff.studio@gmail.com>
2020-12-31 16:36:21 +0500

    1.1. Состояние нити. [#430968]

--- chapter_007/src/main/java/ThreadState.java
+++ chapter_007/src/main/java/ThreadState.java
@@ -1,25 +1,28 @@
 public class ThreadState {
+
     public static void main(String[] args) {
+
         Thread first = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
-        System.out.println(first.getState());
+        System.out.println("Before the start " + first.getName() + ": " + first.getState());
         first.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getState());
-        }
-        System.out.println(first.getState());
 
         Thread second = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
-        System.out.println(second.getState());
+        System.out.println("Before the start " + second.getName() + ": " + second.getState());
         second.start();
-        while (second.getState() != Thread.State.TERMINATED) {
-            System.out.println(second.getState());
+
+        while ((first.getState() != Thread.State.TERMINATED) || (second.getState() != Thread.State.TERMINATED) ) {
+            System.out.println(first.getName() + ": " + first.getState());
+            System.out.println(second.getName() + ": " + second.getState());
         }
-        System.out.println(second.getState());
+
+        System.out.println("After the finish " + first.getName() + ": " + first.getState());
+        System.out.println("After the finish " + first.getName() + ": " + second.getState());
 
         System.out.println("Work completed");
     }
+
 }
